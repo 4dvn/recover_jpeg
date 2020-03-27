68dddc5ea5eb2d7f79c2561c532dfa92b38b9c9e
4dvn <vuha.hau@hyper.vn>
2020-03-28 02:16:19 +0700

    Update build.bat

--- build.bat
+++ build.bat
@@ -1,12 +1,11 @@
-@echo off
+echo off
+Cls
+Color a
 set GOOS=windows
-
 set GOARCH=386
-go build -o ImageSaver_x86.exe -ldflags "-w -s"
-upx ImageSaver_x86.exe
-
+go build -o Repair_x86.exe -ldflags "-w -s"
+upx Repair_x86.exe
 set GOARCH=amd64
-go build -o ImageSaver_x64.exe -ldflags "-w -s"
-upx ImageSaver_x64.exe
-
+go build -o Repair_x64.exe -ldflags "-w -s"
+upx Repair_x64.exe
 pause
